97ad460d948ad63af59a37a9f92ec0542e8d7944
Sky <labidaniele@itis-molinari.eu>
2025-01-19 22:45:38 +0100

    ins

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { tableComponent } from './componenti/tabella.js';
 import { createMap } from './componenti/mappa.js';
 import { loginComponent} from './componenti/login.js';
-import { insertData} from './componenti/inserimento.js';
+import { insertData} from './inserimento.js';
 console.log("ciao");
