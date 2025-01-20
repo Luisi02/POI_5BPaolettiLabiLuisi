5bb3ee2551008e707bf75f872c8957fc28393b1c
lettipao <paolettitommaso@itis-molinari.eu>
2025-01-20 15:48:53 +0100

    login

--- componenti/login.js
+++ componenti/login.js
@@ -1,6 +1,29 @@
 const loginComponent = () => {
-    return () => {
-        console.log('login component loaded');
+    const login = document.getElementById('login');
+    const input_password = document.getElementById('input_password');
+    const input_username = document.getElementById('input_username');
+    const modale_login = document.getElementById('modale_login');
+    const submit_login = document.getElementById('submit_login');
+    return {
+        render: () => {
+            login.innerHTML = "login";
+            window.onclick = () => {
+                if (event.target == modale_login) {
+                    modale_login.style.display = "none";
+                }
+            }
+            login.onclick = () => {
+                modale_login.style.display = "flex";
+                input_password.value = "";
+                input_username.value = "";
+            }
+            submit_login.onclick = () => {
+                if (input_password.value === "password" && input_username.value === "username"){
+                    modale_login.style.display = "none"
+                    login.innerHTML = input_username.value
+                }
+            }
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { tableComponent } from './componenti/tabella.js';
 import { createMap } from './componenti/mappa.js';
 import { loginComponent} from './componenti/login.js';
-console.log("ciao");
+/*import { argumentComponent} from './componenti/argomento.js';*/
+loginComponent().render();
+createMap().render();
